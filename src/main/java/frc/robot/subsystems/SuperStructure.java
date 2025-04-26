542d79b4d564e60a4e5caafecc3948a21f729a3a
ZhangzrJerry <ZhangzrJerry@outlook.com>
2025-04-26 23:19:28 +0800

    Refactor ArmGoal class for improved readability; adjust formatting and annotations for consistency.

--- src/main/java/frc/robot/subsystems/SuperStructure.java
+++ src/main/java/frc/robot/subsystems/SuperStructure.java
@@ -32,25 +32,25 @@ public class SuperStructure {
 
   public Command forcedIdleCmd() {
     return Commands.runOnce(
-        () -> {
-          intake.setGoal(IntakeGoal.IIDLE); // it's not a typo, it's intentional
-          arm.setGoal(ArmGoal.IDLE);
-          intake.setGoal(IntakeGoal.IDLE);
-          endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.IDLE);
-          endeffector.setCoralGoal(CoralEndEffectorGoal.IDLE);
-          swerve.getDefaultCommand();
-        },
-        swerve)
+            () -> {
+              intake.setGoal(IntakeGoal.IIDLE); // it's not a typo, it's intentional
+              arm.setGoal(ArmGoal.IDLE);
+              intake.setGoal(IntakeGoal.IDLE);
+              endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.IDLE);
+              endeffector.setCoralGoal(CoralEndEffectorGoal.IDLE);
+              swerve.getDefaultCommand();
+            },
+            swerve)
         .withName("Super/Forced Idle");
   }
 
   public Command algaeIntakePickCmd() {
     return Commands.run(
-        () -> {
-          arm.setGoal(ArmGoal.ALGAE_GROUND_PICK);
-          intake.setGoal(IntakeGoal.COLLECT);
-          endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.COLLECT);
-        })
+            () -> {
+              arm.setGoal(ArmGoal.ALGAE_GROUND_PICK);
+              intake.setGoal(IntakeGoal.COLLECT);
+              endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.COLLECT);
+            })
         .finallyDo(
             () -> {
               arm.setGoal(ArmGoal.IDLE);
@@ -64,124 +64,124 @@ public class SuperStructure {
     String name = "Super/Algae Reef Pick";
     SmartDashboard.putBoolean("name", highPick);
     return Commands.sequence(
-        Commands.runOnce(
-            () -> {
-              endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.COLLECT);
-              arm.setGoal(highPick ? ArmGoal.ALGAE_HIGH_PICK : ArmGoal.ALGAE_LOW_PICK);
-            },
-            arm)
-            .withName(name + "/Set Pick Goal"),
-        Commands.waitUntil(() -> endeffector.hasAlgaeEndeffectorStoraged())
-            .withName(name + "/Wait For Pick"),
-        Commands.runOnce(
-            () -> {
-              arm.setGoal(ArmGoal.IDLE);
-              endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.HOLDING);
-            })
-            .withName(name + "/Set Holding Goal"))
+            Commands.runOnce(
+                    () -> {
+                      endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.COLLECT);
+                      arm.setGoal(highPick ? ArmGoal.ALGAE_HIGH_PICK : ArmGoal.ALGAE_LOW_PICK);
+                    },
+                    arm)
+                .withName(name + "/Set Pick Goal"),
+            Commands.waitUntil(() -> endeffector.hasAlgaeEndeffectorStoraged())
+                .withName(name + "/Wait For Pick"),
+            Commands.runOnce(
+                    () -> {
+                      arm.setGoal(ArmGoal.IDLE);
+                      endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.HOLDING);
+                    })
+                .withName(name + "/Set Holding Goal"))
         .withName(name);
   }
 
   public Command algaeMagicEjectCmd() {
     return Commands.startEnd(
-        () -> {
-          if (arm.getGoal().equals(ArmGoal.ALGAE_GROUND_PICK)) {
-            intake.setGoal(IntakeGoal.EJECT);
-            endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.EJECT);
-          } else if (arm.getGoal().equals(ArmGoal.ALGAE_NET_SCORE)
-              || arm.getGoal().equals(ArmGoal.ALGAE_PROCESSOR_SCORE)) {
-            endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.SCORE);
-          } else {
-            endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.EJECT);
-          }
-        },
-        () -> endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.IDLE))
+            () -> {
+              if (arm.getGoal().equals(ArmGoal.ALGAE_GROUND_PICK)) {
+                intake.setGoal(IntakeGoal.EJECT);
+                endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.EJECT);
+              } else if (arm.getGoal().equals(ArmGoal.ALGAE_NET_SCORE)
+                  || arm.getGoal().equals(ArmGoal.ALGAE_PROCESSOR_SCORE)) {
+                endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.SCORE);
+              } else {
+                endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.EJECT);
+              }
+            },
+            () -> endeffector.setAlgaeGoal(AlgaeEndEffectorGoal.IDLE))
         .withName("Super/Algae Magic Eject");
   }
 
   public Command algaeNetScoreCmd() {
     String name = "Super/Algae Net Score";
     return Commands.sequence(
-        Commands.runOnce(
-            () -> {
-              arm.setGoal(ArmGoal.ALGAE_NET_SCORE);
-            },
-            arm)
-            .withName(name + "/Set Score Pose"))
+            Commands.runOnce(
+                    () -> {
+                      arm.setGoal(ArmGoal.ALGAE_NET_SCORE);
+                    },
+                    arm)
+                .withName(name + "/Set Score Pose"))
         .withName(name);
   }
 
   public Command algaeProcessorScoreCmd() {
     String name = "Super/Algae Processor Score";
     return Commands.sequence(
-        Commands.runOnce(
-            () -> {
-              arm.setGoal(ArmGoal.ALGAE_PROCESSOR_SCORE);
-            })
-            .withName(name + "/Set Score Pose"),
-        Commands.waitUntil(() -> arm.stopAtGoal()).withName(name + "/Wait For Pose"))
+            Commands.runOnce(
+                    () -> {
+                      arm.setGoal(ArmGoal.ALGAE_PROCESSOR_SCORE);
+                    })
+                .withName(name + "/Set Score Pose"),
+            Commands.waitUntil(() -> arm.stopAtGoal()).withName(name + "/Wait For Pose"))
         .withName(name);
   }
 
   public Command coralStationPickCmd() {
     String name = "Super/Coral Station Pick";
     return Commands.startEnd(
-        () -> {
-          arm.setGoal(ArmGoal.CORAL_STATION_COLLECT);
-          endeffector.setCoralGoal(CoralEndEffectorGoal.COLLECT);
-        },
-        () -> {
-          endeffector.setCoralGoal(CoralEndEffectorGoal.IDLE);
-        })
+            () -> {
+              arm.setGoal(ArmGoal.CORAL_STATION_COLLECT);
+              endeffector.setCoralGoal(CoralEndEffectorGoal.COLLECT);
+            },
+            () -> {
+              endeffector.setCoralGoal(CoralEndEffectorGoal.IDLE);
+            })
         .withName(name);
   }
 
   public Command coralReefScoreCmd(int layer) {
     String name = "Super/Coral Reef Score";
     return Commands.sequence(
-        Commands.runOnce(
-            () -> {
-              switch (layer) {
-                case 1:
-                  arm.setGoal(ArmGoal.CORAL_L1_SCORE);
-                  break;
-                case 2:
-                  arm.setGoal(ArmGoal.CORAL_L2_SCORE);
-                  break;
-                case 3:
-                  arm.setGoal(ArmGoal.CORAL_L3_SCORE);
-                  break;
-                case 4:
-                  arm.setGoal(ArmGoal.CORAL_L4_SCORE);
-                  break;
-              }
-            })
-            .withName(name + "/Set Score Pose"))
+            Commands.runOnce(
+                    () -> {
+                      switch (layer) {
+                        case 1:
+                          arm.setGoal(ArmGoal.CORAL_L1_SCORE);
+                          break;
+                        case 2:
+                          arm.setGoal(ArmGoal.CORAL_L2_SCORE);
+                          break;
+                        case 3:
+                          arm.setGoal(ArmGoal.CORAL_L3_SCORE);
+                          break;
+                        case 4:
+                          arm.setGoal(ArmGoal.CORAL_L4_SCORE);
+                          break;
+                      }
+                    })
+                .withName(name + "/Set Score Pose"))
         .withName(name);
   }
 
   public Command coralMagicEjectCmd() {
     return Commands.startEnd(
-        () -> {
-          if (arm.getGoal().equals(ArmGoal.CORAL_L1_SCORE)
-              || arm.getGoal().equals(ArmGoal.CORAL_L2_SCORE)
-              || arm.getGoal().equals(ArmGoal.CORAL_L3_SCORE)
-              || arm.getGoal().equals(ArmGoal.CORAL_L4_SCORE)) {
-            endeffector.setCoralGoal(CoralEndEffectorGoal.SCORE);
-          } else {
-            endeffector.setCoralGoal(CoralEndEffectorGoal.EJECT);
-          }
-        },
-        () -> endeffector.setCoralGoal(CoralEndEffectorGoal.IDLE))
+            () -> {
+              if (arm.getGoal().equals(ArmGoal.CORAL_L1_SCORE)
+                  || arm.getGoal().equals(ArmGoal.CORAL_L2_SCORE)
+                  || arm.getGoal().equals(ArmGoal.CORAL_L3_SCORE)
+                  || arm.getGoal().equals(ArmGoal.CORAL_L4_SCORE)) {
+                endeffector.setCoralGoal(CoralEndEffectorGoal.SCORE);
+              } else {
+                endeffector.setCoralGoal(CoralEndEffectorGoal.EJECT);
+              }
+            },
+            () -> endeffector.setCoralGoal(CoralEndEffectorGoal.IDLE))
         .withName("Super/Coral Magic Eject");
   }
 
   public Command setClimbingModeCmd() {
     return Commands.runOnce(
-        () -> {
-          climber.setGoal(ClimberGoal.READY);
-          arm.setGoal(ArmGoal.CLIMB);
-        })
+            () -> {
+              climber.setGoal(ClimberGoal.READY);
+              arm.setGoal(ArmGoal.CLIMB);
+            })
         .withName("Super/Set Climbing");
   }
 }
--- src/main/java/frc/robot/subsystems/arm/ArmGoal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmGoal.java
@@ -5,45 +5,53 @@ import java.util.Map;
 import lombok.Getter;
 
 public class ArmGoal extends TunableNumbers {
-    public static final String SHOULDER_HEIGHT_METER = "ShoulderHeightMeter";
-    public static final String ELBOW_POSITION_DEGREE = "ElbowPositionDegree";
+  public static final String SHOULDER_HEIGHT_METER = "ShoulderHeightMeter";
+  public static final String ELBOW_POSITION_DEGREE = "ElbowPositionDegree";
 
-    @Getter
-    private final String name;
+  @Getter private final String name;
 
-    public ArmGoal(String baseKey, double shoulderHeightMeter, double elbowPositionDegree) {
-        super(
-                baseKey,
-                Map.of(
-                        SHOULDER_HEIGHT_METER, shoulderHeightMeter,
-                        ELBOW_POSITION_DEGREE, elbowPositionDegree));
-        this.name = baseKey.substring(baseKey.lastIndexOf("/") + 1);
-    }
+  public ArmGoal(String baseKey, double shoulderHeightMeter, double elbowPositionDegree) {
+    super(
+        baseKey,
+        Map.of(
+            SHOULDER_HEIGHT_METER, shoulderHeightMeter,
+            ELBOW_POSITION_DEGREE, elbowPositionDegree));
+    this.name = baseKey.substring(baseKey.lastIndexOf("/") + 1);
+  }
 
-    public double getShoulderHeightMeter() {
-        return get(SHOULDER_HEIGHT_METER);
-    }
+  public double getShoulderHeightMeter() {
+    return get(SHOULDER_HEIGHT_METER);
+  }
 
-    public double getElbowPositionDegree() {
-        return get(ELBOW_POSITION_DEGREE);
-    }
+  public double getElbowPositionDegree() {
+    return get(ELBOW_POSITION_DEGREE);
+  }
 
-    public static final ArmGoal START = new ArmGoal("Arm/Goal/Start", 0.0, 107.0);
-    public static final ArmGoal IDLE = new ArmGoal("Arm/Goal/Idle", 0.2, -65.0);
-    public static final ArmGoal HOME = new ArmGoal("Arm/Goal/Home", 0.0, 0.0);
-    public static final ArmGoal CLIMB = new ArmGoal("Arm/Goal/Climb", 0.7, -180.0);
+  public static final ArmGoal START = new ArmGoal("Arm/Goal/Start", 0.0, 107.0);
+  public static final ArmGoal IDLE = new ArmGoal("Arm/Goal/Idle", 0.2, -65.0);
+  public static final ArmGoal HOME = new ArmGoal("Arm/Goal/Home", 0.0, 0.0);
+  public static final ArmGoal CLIMB = new ArmGoal("Arm/Goal/Climb", 0.7, -180.0);
 
-    public static final ArmGoal CORAL_L1_SCORE = new ArmGoal("Arm/Goal/CoralL1Score", 0.4 - 0.04, -32.0);
-    public static final ArmGoal CORAL_L2_SCORE = new ArmGoal("Arm/Goal/CoralL2Score", 0.64 - 0.04, -32.0);
-    public static final ArmGoal CORAL_L3_SCORE = new ArmGoal("Arm/Goal/CoralL3Score", 1.06 - 0.04, -34.0);
-    public static final ArmGoal CORAL_L4_SCORE = new ArmGoal("Arm/Goal/CoralL4Score", 1.7 - 0.04, -34.0);
-    public static final ArmGoal CORAL_STATION_COLLECT = new ArmGoal("Arm/Goal/CoralStationCollect", 0.01, -232.0);
+  public static final ArmGoal CORAL_L1_SCORE =
+      new ArmGoal("Arm/Goal/CoralL1Score", 0.4 - 0.04, -32.0);
+  public static final ArmGoal CORAL_L2_SCORE =
+      new ArmGoal("Arm/Goal/CoralL2Score", 0.64 - 0.04, -32.0);
+  public static final ArmGoal CORAL_L3_SCORE =
+      new ArmGoal("Arm/Goal/CoralL3Score", 1.06 - 0.04, -34.0);
+  public static final ArmGoal CORAL_L4_SCORE =
+      new ArmGoal("Arm/Goal/CoralL4Score", 1.7 - 0.04, -34.0);
+  public static final ArmGoal CORAL_STATION_COLLECT =
+      new ArmGoal("Arm/Goal/CoralStationCollect", 0.01, -232.0);
 
-    public static final ArmGoal ALGAE_LOW_PICK = new ArmGoal("Arm/Goal/AlgaeLowPick", 0.2, -131.0);
-    public static final ArmGoal ALGAE_HIGH_PICK = new ArmGoal("Arm/Goal/AlgaeHighPick", 0.6, -132.0);
-    public static final ArmGoal ALGAE_AUTO_LOW_PICK = new ArmGoal("Arm/Goal/AlgaeAutoLowPick", 0.5, -90.0);
-    public static final ArmGoal ALGAE_AUTO_HIGH_PICK = new ArmGoal("Arm/Goal/AlgaeAutoHighPick", 0.7, -90.0);
-    public static final ArmGoal ALGAE_PROCESSOR_SCORE = new ArmGoal("Arm/Goal/AlgaeProcessorScore", 0.0, -180.0);
-    public static final ArmGoal ALGAE_NET_SCORE = new ArmGoal("Arm/Goal/AlgaeNetScore", 1.5, -45.0);
-    public static final ArmGoal ALGAE_GROUND_PICK = new ArmGoal("Arm/Goal/AlgaeGroundPick", 0.25, 45.0);
+  public static final ArmGoal ALGAE_LOW_PICK = new ArmGoal("Arm/Goal/AlgaeLowPick", 0.2, -131.0);
+  public static final ArmGoal ALGAE_HIGH_PICK = new ArmGoal("Arm/Goal/AlgaeHighPick", 0.6, -132.0);
+  public static final ArmGoal ALGAE_AUTO_LOW_PICK =
+      new ArmGoal("Arm/Goal/AlgaeAutoLowPick", 0.5, -90.0);
+  public static final ArmGoal ALGAE_AUTO_HIGH_PICK =
+      new ArmGoal("Arm/Goal/AlgaeAutoHighPick", 0.7, -90.0);
+  public static final ArmGoal ALGAE_PROCESSOR_SCORE =
+      new ArmGoal("Arm/Goal/AlgaeProcessorScore", 0.0, -180.0);
+  public static final ArmGoal ALGAE_NET_SCORE = new ArmGoal("Arm/Goal/AlgaeNetScore", 1.5, -45.0);
+  public static final ArmGoal ALGAE_GROUND_PICK =
+      new ArmGoal("Arm/Goal/AlgaeGroundPick", 0.25, 45.0);
 }
